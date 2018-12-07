59fb47823c4c890effffc8b65d63c49fa4069dcc
ime <fuwei880306@gmail.com>
2018-12-07 10:33:44 +0900

    V1.1

--- app.js
+++ app.js
@@ -4,7 +4,16 @@
 var express = require('express');
 //创建app应用,相当于nodeJS的http.createService()
 var app = express();
- 
+
+var swig = require('swig');
+
+app.engine('html',swig.renderFile);
+app.set('views','./views');
+app.set('view engine','html');
+swig.setDefaults({cache:false});
+
+app.use('/public',express.static(__dirname+'/public'));
+
 /**
  * [description] 给app绑定首页路由，把一个url路径通过一个或多个方法绑定
  * @param  {[type]} req       request对象，保存客户端请求相关的一些数据
@@ -14,8 +23,13 @@ var app = express();
  */
 app.get('/',function(req,res,next){
 	//res.send(string)发送内容直客户端
-	res.send('<h1>欢迎来到我的博客！</h1>')
+	res.render('index');
 })
- 
+
+app.get('/main.css',function(req,res,next){
+	res.setHeader('content-type','text/css');
+	res.send("body {color:red}");
+});
+
 //监听http请求
 app.listen(8081);
